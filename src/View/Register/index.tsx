a1b1567a5f335363e71a1c63554d816a08aadadf
lu97chi <lu97is@gmail.com>
2020-04-12 17:42:59 -0600

    fix register

--- src/View/Register/index.tsx
+++ src/View/Register/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Typography, TextField } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 import { ParticlesContainer, RegisterContainer, SignText } from './styled';
@@ -7,6 +7,7 @@ import { InputType } from '../Login';
 import { Big, Medium, Small } from '../../theme/spacing';
 import CustomButton from '../../components/CustomButton';
 import { useResize } from '../../helpers/hooks';
+import { Caller } from '../../api/request';
 
 const inputs: Array<InputType> = [
   { name: 'username' },
@@ -19,6 +20,24 @@ const inputs: Array<InputType> = [
 const Register = () => {
   const { push } = useHistory();
   const { windowDimensions } = useResize();
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: '',
+    email: '',
+    'First Name': '',
+    'Last Name': '',
+  });
+  const handleChange = (value: string, option: string) => {
+    setCredentials((prev) => ({ ...prev, [option]: value }));
+  };
+
+  const register = () => {
+    Caller({
+      api: 'posts/1',
+      method: 'get',
+      callback: (data: any) => console.log(data),
+    });
+  };
   return (
     <RegisterContainer>
       <ParticlesContainer
@@ -37,7 +56,7 @@ const Register = () => {
         height="60%"
         width={windowDimensions.width <= 960 ? '80%' : '40%'}
         elevation={5}
-        handleEnterPress={() => console.log('ye')}
+        handleEnterPress={() => register()}
       >
         <Typography variant="h5">Register</Typography>
         {inputs.map(({ name, type = 'string' }) => (
@@ -48,13 +67,14 @@ const Register = () => {
             label={name}
             type={type}
             variant="outlined"
+            onChange={(e) => handleChange(e.target.value, name)}
           />
         ))}
         <CustomButton
           label="Register"
           fullWidth
           margin={`${Big} 0px ${Small} 0px`}
-          handleClick={() => console.log('yei')}
+          handleClick={() => register()}
           type="primary"
           padding={`${Medium}`}
         />
