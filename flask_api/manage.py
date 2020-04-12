011f5602afc868c50a3a1e498ac621c7ef275169
Marckhz <marcohdes94i@gmail.com>
2020-04-12 17:04:24 -0500

    fix localhost

--- flask_api/manage.py
+++ flask_api/manage.py
@@ -2,8 +2,6 @@ from app import create_app
 from flask_script import Manager, Server
 from config import config
 
-from app.views import email_survey_notification
-
 import time
 
 import datetime
@@ -13,15 +11,7 @@ import datetime
 config_class = config['development']
 app = create_app(config_class)
 
-
-def send_emails():
-	with app.app_context():
-		email_survey_notification()
-
-
-
 if __name__ == '__main__':
 	manager = Manager(app)
-	#manager.add_command("createsuperuser", createsuperuser)
-	manager.add_command("runserver", Server('192.168.31.162') )
+	manager.add_command("runserver", Server('0.0.0.0') )
 	manager.run()
